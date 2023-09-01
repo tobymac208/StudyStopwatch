9dae916f2b49240e6097b7c4221bd39768b68109
tobymac208 <niklas_fernandez@comcast.net>
2023-09-01 14:08:37 -0500

    Ackowledges what the user wants to study, how long, and for how many times.

--- run.py
+++ run.py
@@ -119,6 +119,9 @@ def main():
 
     # Returns a tuple of required information.
     repetitions, minutes, subject = ask_user_for_control_variables()
+    
+    # Print control variables as a response to the user's input. The lets them know what they entered.
+    print(f'You want to study for {minutes}, {repetitions} times. You want to study for {subject}. Thanks!')
 
     # Run the loop for the amount of repetitions specified.
     for i in range(repetitions):
