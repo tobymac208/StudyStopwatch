663c0958dceed8dfa79fc594ac47563d866f5432
tobymac208 <niklas_fernandez@comcast.net>
2024-05-25 15:35:35 -0500

    noice

--- web_interface/js/master.js
+++ web_interface/js/master.js
@@ -5,15 +5,19 @@ let started = false;
 const timer = () => {
   var start = Date.now();
   started = true;
+  document.getElementById("currenttime").style.color = "red";
 
   let inter = setInterval(() => {
     var delta = Date.now() - start; // milliseconds elapsed since start
     secondsElapsed = Math.floor(delta / 1000);
 
-    timeTracker++;
-    document.getElementById("currenttime").value = timeTracker;
+    // check if a minute has passed
+    if (secondsElapsed >= 60) {
+      document.getElementById("currenttime").value = ++timeTracker;
+    }
 
-    if (secondsElapsed >= minutes || !started) {
+    if (timeTracker >= minutes || !started) {
+      stop();
       clearInterval(inter); // stop the interval, and the timer.
     }
 
@@ -22,9 +26,13 @@ const timer = () => {
   }, 1000); // update about every second
 };
 
+const stop = () => {
+    started = false;
+    document.getElementById("currenttime").style.color = "black";
+};
+
 const reset = () => {
-  started = false;
+  stop();
   timeTracker = 0;
   document.getElementById("currenttime").value = timeTracker;
-  document.getElementById("currenttime").style.color = "black";
 };
