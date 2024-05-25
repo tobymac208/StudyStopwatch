588d9f95f44b1f658e9527f7791dfbd60c33ee24
tobymac208 <niklas_fernandez@comcast.net>
2024-05-25 06:20:33 -0500

    Adds a control variable to ensure the user has studied their desired time. Not used yet.

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-DESIRED_BREAK_TIME = 5 # Specify how long a break should be in minutes.
+DESIRED_BREAK_TIME =        5 # Specify how long a break should be in minutes.
+DESIRED_DAILY_STUDY_TIME =  120 # The goal study time for each day (in minutes).
